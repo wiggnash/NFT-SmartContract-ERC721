8273ff092114c8ee5e4143b280b21bc6a7d5dc1c
wigg_nash <vigneshwar.sigamani@gmail.com>
2023-05-23 12:53:52 +0530

    Finished writing my nft smart contract

---
+++ README.md
@@ -0,0 +1,14 @@
+# Developing an NFT Smart Contract (ERC721)
+
+We will develop and deploy an ERC721 (NFT) smart contract using 
+1. Alchemy
+2. OpenZeppelin 
+3. Remix 
+4. Ethereum Sepolia.
+
+## Target
+
+1. Write and modify the smart contract using OpenZeppelin
+2. Deploy it on the Ethereum sepolia testnet blockchain to save on gas fees
+3. Host the NFT tokens metadata on IPFS using Filebase
+4. Mint an NFT and visualize it on OpenSea.
--- contracts/myNFT.sol
+++ contracts/myNFT.sol
@@ -4,24 +4,33 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Wigg_nash is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
-    using Counters for Counters.Counter;
+contract Wigg_nash is ERC721, ERC721Enumerable, ERC721URIStorage {
+    using Counters for Counters.Counter; //initialising counters library that we will use track our NFT id's
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("wigg_nash", "WIGG") {}
+    // To limit the Max number of NFT's tjat users will be able to mint
+    uint256 MAX_SUPPLY = 10000;
 
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
+    constructor() ERC721("Wigg_nash", "WIGG") {} //assiging name and the symbol of our smart contract 
+
+    //Miting the NFT here 
+    // 1. Address of the wallet or the smart contract that we want to send the NFT to 
+    // 2. Properties of the NFT which have to be sent to the address of the smart contact 
+     
+    function safeMint(address to, string memory uri) public{
+        uint256 tokenId = _tokenIdCounter.current(); // giving the current token ID number to the token ID
+        // checking the max supply with the tokenId , If the TokenId is greater than the max supply , display the error message
+        require(tokenId <= MAX_SUPPLY, "I'm Sorry All the NFT's are Minted");
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
     // The following functions are overrides required by Solidity.
+    // Function overloading - We will rewrite the function in our child smart contract 
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
@@ -34,6 +43,7 @@ contract Wigg_nash is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         super._burn(tokenId);
     }
 
+    // this is the function in which the opensea and other marketplaces will call and gather the URI of the token
     function tokenURI(uint256 tokenId)
         public
         view
